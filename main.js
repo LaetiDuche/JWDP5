f4d8cdba25713a7eda0feea73c4c119b050cc569
LaetiDuche <79939699+LaetiDuche@users.noreply.github.com>
2021-08-05 14:33:23 +0200

    ajout form page panier +modif css js

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 fetch('http://localhost:3000/api/teddies');
 
+//Validation formulaire commande
+(function () {
+    'use strict'
+  
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+

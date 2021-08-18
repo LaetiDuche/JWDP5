9f62b48e536fb3cb1dcada8e21d6b553bfa3e96d
LaetiDuche <79939699+LaetiDuche@users.noreply.github.com>
2021-08-18 18:56:23 +0200

    test nouveau code js index dom

--- index.js
+++ index.js
@@ -1,4 +1,78 @@
-/* const { response } = require("express"); */
+console.log(window);
+/* alert('test'); */
+
+let container = document.getElementById('card-teddy');
+
+let firstDiv = document.createElement('div');
+container.appendChild(firstDiv);
+document.querySelector("div").classList.add('row');
+
+let articleTeddy = document.createElement('article');
+firstDiv.appendChild(articleTeddy);
+document.querySelector("article").classList.add('col-12', 'col-md-6', 'col-xl-4', 'my-3');
+
+let lienTeddy = document.createElement('a');
+articleTeddy.appendChild(lienTeddy);
+document.querySelector("article > a").setAttribute('href', 'produit.html');
+
+let figureTeddy = document.createElement('figure');
+lienTeddy.appendChild(figureTeddy);
+document.querySelector("figure").classList.add('card','rounded-3', 'my-1', 'shadow-sm');
+
+let imageTeddy = document.createElement('img');
+figureTeddy.appendChild(imageTeddy);
+document.querySelector("figure >img").classList.add('card-img-top');
+document.querySelector("figure > img").setAttribute("alt", "");
+
+let captionTeddy = document.createElement('figcaption');
+figureTeddy.appendChild(captionTeddy);
+document.querySelector("figcaption").classList.add('card-body');
+
+let textTeddy = document.createElement('div');
+captionTeddy.appendChild(textTeddy);
+document.querySelector("figcaption > div").classList.add("title-price" ,"d-flex");
+
+let nomTeddy = document.createElement('h2');
+textTeddy.appendChild(nomTeddy);
+document.querySelector('h2').setAttribute('id', 'name');
+
+let prixTeddy = document.createElement('p');
+textTeddy.appendChild(prixTeddy);
+document.querySelector('figcaption > div > p').classList.add("card-text", "my-auto");
+
+let descriptionTeddy = document.createElement('p');
+captionTeddy.append(descriptionTeddy);
+document.querySelector('figure > figcaption > p').setAttribute("id", "description");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* fetch('http://localhost:3000/api/teddies')
   .then((response) => response.json())
@@ -7,24 +81,53 @@
     console.log(error)
   }); */
 
-var urlTeddy = 'http://localhost:3000/api/teddies';
+/* var urlTeddy = 'http://localhost:3000/api/teddies';
 
 fetch(urlTeddy)
   .then((response) =>
-    response.json().then((data) => {
-      console.log(data);
-      document.querySelector('#image').innerHTML = data.imageUrl;
-      document.querySelector('#name').innerHTML = data.name;
-      document.querySelector('#price').innerHTML = data.price + ' €';
-      document.querySelector('#description').innerHTML = data.description;
-    })
+    response.json()
+      .then((data) => {
+        console.log(data);
+        document.querySelector('#image').innerHTML = data.imageUrl;
+        document.querySelector('#name').innerHTML = data.name;
+        document.querySelector('#price').innerHTML = data.price + ' €';
+        document.querySelector('#description').innerHTML = data.description;
+
+      })
   )
-  .catch((err) => console.log('erreur : ' + err));
+  .catch((err) => console.log('erreur : ' + err)); */
+
+/*   function getTeddy() {
+    fetch("http://localhost:3000/api/teddies")
+    .then(function(res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(function(value) {
+      document
+          .getElementById("name")
+          .innerText = value.name.Norbert;
+    })
+    .catch(function(err) {
+      // Une erreur est survenue
+    });
+  } */
+/* const params = new URLSearchParams(window.location.search);
+
+const values = params.get('values'); // will return the value of the parameter page
+console.log(values) */
+
+
+/* var imageTeddy = window.location.search; */
+/* var container = document.getElementById('image'); */
+/* container.innerHTML = imageTeddy; */
+
 
 
 //Création de l'article des teddies
 
-var cardTeddy = document.getElementById('card-teddy');
+/* var cardTeddy = document.getElementById('card-teddy');
 cardTeddy.innerHTML = "<div><article><a><figure><img></img><figcaption><div><h2></h2><p></p></div><p></p></figcaption></figure></a></article></div>";
 
 var rowTeddy = document.querySelector("#card-teddy > div");
@@ -35,7 +138,7 @@ articleTeddy.setAttribute("class", "col-12 col-md-6 col-xl-4 my-3 ");
 articleTeddy.setAttribute("id", "");
 
 var lienTeddy = document.querySelector("#card-teddy > div >article > a");
-lienTeddy.setAttribute("href", "");
+lienTeddy.setAttribute("href", "produit.html");
 
 var figureTeddy = document.querySelector("#card-teddy > div >article > a > figure");
 figureTeddy.setAttribute("class", "card rounded-3 my-1 shadow-sm ");
@@ -44,6 +147,7 @@ var imageTeddy = document.querySelector("#card-teddy > div >article > a > figure
 imageTeddy.setAttribute("class", "card-img-top ");
 imageTeddy.setAttribute("alt", "");
 imageTeddy.setAttribute("id", "image");
+
 
 var captionTeddy = document.querySelector("#card-teddy > div >article > a > figure > figcaption");
 captionTeddy.setAttribute("class", "card-body");
@@ -60,5 +164,5 @@ prixTeddy.setAttribute("id", "price");
 
 var descriptionTeddy = document.querySelector("#card-teddy > div >article > a > figure > figcaption > p");
 descriptionTeddy.setAttribute("id", "description");
-
+ */
 

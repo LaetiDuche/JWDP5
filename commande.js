4f3ef3bf62165d4e5b187902bacda70079c06646
LaetiDuche <79939699+LaetiDuche@users.noreply.github.com>
2021-09-02 16:24:16 +0200

    modif message confirmation js

--- commande.js
+++ commande.js
@@ -1,6 +1,20 @@
-fetch('http://localhost:3000/api/teddies')
-  .then((response) => response.json())
-  .then(response => console.log(response))
-  .catch(error => {
-    console.log(error)
-  });
+//Fonction pour le message de validation de commande
+
+body();
+function body() {
+  messageValidation();
+}
+
+function messageValidation(){
+  var message = document.getElementById('confirm-message');
+
+  var confirm = document.createElement('h2');
+  message.appendChild(confirm);
+  confirm.classList.add('col-12', 'bg-white', 'p-4', 'rounded-3', 'my-2', 'text-center');
+  confirm.innerHTML = "Votre commande a été envoyé avec succés ! <br> <br> Merci pour votre commande !";
+  
+
+
+
+
+}

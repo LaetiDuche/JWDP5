bab8893176ff7114e429761c40e06e0d180cb5aa
LaetiDuche <79939699+LaetiDuche@users.noreply.github.com>
2021-11-03 23:24:34 +0100

    modif html page commande

--- front/commande.js
+++ front/commande.js
@@ -1,37 +1,27 @@
-//Fonction pour le message de validation de commande
+//-----------------MESSAGE DE VALIDATION DE COMMANDE
 
 body();
 function body() {
-  messageValidation();
-  
+  messageValidation()
+
 }
 
 
-function messageValidation(){
-  let message = document.getElementById('confirm-message');
+let localPanier = JSON.parse(localStorage.getItem("Total", "orderId"));
 
+function messageValidation() {
+  /* let params = new URL(document.location).searchParams;
+  let orderid = params.get("orderId");
+  let total = params.get("total"); */
+  let localPanier = JSON.parse(localStorage.getItem("Total", "orderId"));
+  const total = insertTotal;
+
+  let message = document.getElementById('confirm-message');
+ 
   let confirmId = document.createElement('p');
   message.appendChild(confirmId);
   confirmId.classList.add('col-12', 'bg-white', 'p-2', 'rounded-3', 'my-2', 'text-center');
-  confirmId.innerHTML = "Votre commande n° ";
+  confirmId.innerHTML = `Votre commande n° ${orderid} , </br> au prix de ${total} € , a été envoyée avec succés! </br></br> Merci pour votre commande !`;
 
-  let getId = document.createElement('span');
-  confirmId.appendChild(getId);
-  getId.setAttribute('id', 'orderId');
-  
-  let confirmPrice = document.createElement('p');
-  confirmId.appendChild(confirmPrice);
-  confirmPrice.innerHTML = "au prix de "  ;
-  
-  let getTotal = document.createElement('span');
-  confirmId.appendChild(getTotal);
-  getTotal.setAttribute('id', 'subtotal');
-  document.getElementById('subtotal').textContent = + ' €';
-
-  let succes = document.createElement('p');
-  confirmId.appendChild(succes);
-  succes.innerHTML = "a été envoyé avec succés! <br> Merci pour votre commande !";
-  
- 
 }
 

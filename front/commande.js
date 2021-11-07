b1f5fbe2e52558eb5102fd2cd3b940fa933bbeee
LaetiDuche <79939699+LaetiDuche@users.noreply.github.com>
2021-11-07 18:54:00 +0100

    modifs commande

--- front/commande.js
+++ front/commande.js
@@ -10,15 +10,13 @@ function messageValidation() {
   
   //Récupération du Total et de l'orderId dans le localstorage
   let orderId = localStorage.getItem("orderId");
-  let Total = JSON.parse(localStorage.getItem('Total'));
+  let Total = localStorage.getItem("Total");
 
-  //Insertion du Total et de l'orderId dans le html
+  //Insertion du Total et de l'orderId dans le message
   let message = document.getElementById('confirm-message');
 
   let confirmId = document.createElement('p');
   message.appendChild(confirmId);
   confirmId.classList.add('col-12', 'bg-white', 'p-2', 'rounded-3', 'my-2', 'text-center');
   confirmId.innerHTML = `<strong>Merci pour votre commande !</strong></br></br>Votre commande n° <strong>${orderId}</strong> , </br> d'un montant total de <strong>${Total} </strong>, a été envoyée avec succés! </br></br> A bientôt sur Orinours !`;
-
 }
-

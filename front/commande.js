bc14d672151a141de84ebf7dbcf2fa13c7e4825f
LaetiDuche <79939699+LaetiDuche@users.noreply.github.com>
2021-11-09 17:45:25 +0100

    modifs fichiers commande css+bouton+ ajout nom prenom

--- front/commande.js
+++ front/commande.js
@@ -8,15 +8,44 @@ function body() {
 
 function messageValidation() {
 
-  //Récupération du Total et de l'orderId dans le localstorage
+  //Récupération du Total, nom, prénom et de l'orderId dans le localstorage
   let orderId = localStorage.getItem("orderId");
   let Total = localStorage.getItem("Total");
+  let Prenom = JSON.parse(localStorage.getItem("contact")).firstName;
+  let Nom = JSON.parse(localStorage.getItem("contact")).lastName;
+  
 
   //Insertion du Total et de l'orderId dans le message
   let message = document.getElementById('confirm-message');
 
+  console.log('contact');
+
+  //Message
   let confirmId = document.createElement('p');
   message.appendChild(confirmId);
-  confirmId.classList.add('col-12', 'bg-white', 'p-2', 'rounded-3', 'my-2', 'text-center');
-  confirmId.innerHTML = `<strong>Merci pour votre commande !</strong></br></br>Votre commande n° <strong>${orderId}</strong> , </br> d'un montant total de <strong>${Total} </strong>, a été envoyée avec succés! </br></br> A bientôt sur Orinours !`;
+  confirmId.classList.add('col-12', 'bg-white', 'py-4','my-4', 'rounded-3',  'text-center');
+  confirmId.innerHTML =
+    `Merci <strong> ${Prenom} ${Nom} </strong> pour votre commande !</br>
+  </br>Votre commande n° <strong>${orderId}</strong> , 
+  </br> d'un montant total de <strong>${Total} </strong>, a été envoyée avec succés! </br>
+  </br> A bientôt sur Orinours !`;
+
+  //-----Bouton revenir à l'accueil
+  let divRetour = document.createElement('div');
+  confirmId.append(divRetour);
+  divRetour.classList.add('col-12', 'text-center', 'justify-content-around', 'd-flex', 'my-4');
+
+  let btnRetour = document.createElement('button');
+  divRetour.appendChild(btnRetour);
+  btnRetour.classList.add('btn', 'btn-outline-light', 'rounded-3', 'shadow', 'btn-sm');
+  btnRetour.textContent = 'Revenir à l\'accueil';
+  btnRetour.setAttribute('type', 'submit');
+  btnRetour.setAttribute('id', 'btn-revenir');
+
+ let btnReturn = document.querySelector('#btn-revenir');
+ btnReturn.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.location.href = "index.html";
+    localStorage.clear();
+  })
 }
